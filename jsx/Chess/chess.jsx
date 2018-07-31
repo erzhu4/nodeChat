3d0477d50a9a912f862e0c8987ea9f407d67cbab
erzhu4@gmail.com <erzhu4@gmail.com>
2018-07-31 09:51:43 -0400

    make chess component

--- jsx/Chess/chess.jsx
+++ jsx/Chess/chess.jsx
@@ -1,2 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import ChessGame from 'ez-chess';
+
+var io = require('socket.io-client');
+
+export default class ChessComponent extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+        	chats: [],
+        	chessGame: null,
+        	socket: null
+        };
+        this.state.chessGame = new ChessGame();
+        this.state.socket = io.connect();
+    }
+
+    render() {
+    	return (
+            <div className="container">
+                <div className="row">
+
+                </div>
+            </div>
+		);
+    }
+}
+
+if (document.getElementById('chess-container')) {
+    ReactDOM.render(<ChessComponent />, document.getElementById('chess-container'));
+}
